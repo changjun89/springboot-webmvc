edbb28fd08d437e04ccb06965a590fee3c546ebd
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-22 00:07:44 +0900

    11. @SessionAttributes, 멀티 폼 서브밋

--- src/main/java/me/anpan/anpanwebmvc/web/controller/HandlerController.java
+++ src/main/java/me/anpan/anpanwebmvc/web/controller/HandlerController.java
@@ -6,59 +6,61 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("member")
 public class HandlerController {
 
-    @GetMapping("/member/{id}")
-    @ResponseBody
-    public Member getMemmber(@PathVariable("id") Integer id) {
-        Member member = new Member();
-        member.setId(id);
-        return member;
+    @GetMapping("/member/form/name")
+    public String memberForm(Model model) {
+        model.addAttribute("member",new Member());
+        return "member/form-name";
     }
 
-    @GetMapping("/member/form")
-    public String memberForm( Model model) {
-        Member member = new Member();
-        member.setName("changjun");
-        member.setTall(170);
-        model.addAttribute("member",member);
-        return "member/form";
-    }
-
-
-    @PostMapping("/member")
-    public String  getMemmberByName(@Validated @ModelAttribute Member member , BindingResult bindingResult,Model model) {
+    @PostMapping("/member/form/name")
+    public String  memmberFormNameSubmit(@Validated @ModelAttribute Member member , BindingResult bindingResult,Model model) {
         if(bindingResult.hasErrors()){
-            return "/member/form";
+            return "/member/form-name";
         }
+        return "redirect:/member/form/tall";
+    }
 
-//        List<Member> memberList = new ArrayList<Member>();
-//        memberList.add(member);
-//        model.addAttribute(memberList);
-        //save 로직
+    @GetMapping("/member/form/tall")
+    public String memberFormTall(@ModelAttribute Member member,Model model){
+        model.addAttribute("member",member);
+        return "/member/form-tall";
+    }
 
+    @PostMapping("/member/form/tall")
+    public String  memmberFormTallSubmit(@Validated @ModelAttribute Member member , BindingResult bindingResult,Model model,SessionStatus sessionStatus) {
+        if(bindingResult.hasErrors()){
+            return "/member/form-tall";
+        }
+        sessionStatus.setComplete();
         return "redirect:/member/list";
     }
 
     @GetMapping("/member/list")
-    public String getMembers(Model model){
-        //select 로직
+    public String getMembers(Model model) {
         Member member = new Member();
         member.setName("spring");
         member.setTall(100);
 
         List<Member> memberList = new ArrayList<>();
         memberList.add(member);
-        model.addAttribute(memberList);
+
+        model.addAttribute("memberList",memberList);
 
         return "/member/list";
     }
+
+
+
 
 
 
